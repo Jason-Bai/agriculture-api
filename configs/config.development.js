adb267689e74ce1c420eca9481df11e7b81c56d0
jason1213 <jason1213@gmail.com>
2016-09-04 20:00:01 +0800

    remove db configs

--- configs/config.development.js
+++ configs/config.development.js
@@ -1,8 +1,3 @@
 var config = require('./base');
 
-config.db.host = '101.200.181.218';
-config.db.port = 27017;
-config.db.name = 'agriculture';
-config.db.pass = 'ds*hjZ2u^BLHIHk0';
-
 module.exports = config;
