763b3a7fced3673ec1b932f6dc225f7ae3e31494
zaohuayudie <atlatl333@126.com>
2016-10-26 10:18:28 +0800

    ..

--- routes/category.js
+++ routes/category.js
@@ -10,25 +10,5 @@ router.get('/', Category.findAll);
 
 router.post('/', Category.create);
 
-<<<<<<< HEAD:routes/categories.js
 router.get('/:id', Category.findOne);
-=======
-  var name = req.body.name + Math.random() * 10000;
 
-  Category.create(name, function (err, category) {
-    res.redirect('/categories');
-  });
-
-
-});
-
-router.get('/:id', function (req, res) {
-  var obj = {
-    id: 'xxxx' + Math.random() * 10000,
-    name: '水果'
-  };
-  res.json(obj);
-});
-
-module.exports = router;
->>>>>>> 153d04f404499dd47cf98b4a55ac464f4cdc9138:routes/category.js
